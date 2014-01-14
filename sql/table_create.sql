2783527d8404c533007cf4ea6786626547df1a2e
liuyijiang <liuyijiang3430@qq.com>
2014-01-14 23:26:40 +0800

    dawdw

--- sql/table_create.sql
+++ sql/table_create.sql
@@ -6,6 +6,8 @@ CREATE  TABLE `mxk`.`TB_WEB_RESOURCE` (
   `sitename` VARCHAR(45) NULL ,
   `siteurl` VARCHAR(250) NULL ,
   `url` VARCHAR(250) NULL ,
+  `info` VARCHAR(650) NULL ,
+  `multiinfo` VARCHAR(250) NULL ,
   `reads` INT NULL ,
   `posts` INT NULL ,
   `insignificance` INT NULL ,
