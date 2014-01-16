28bbefaedc6c30296df0e0b927e37dfd1c5500bf
liuyijiang <liuyijiang3430@qq.com>
2014-01-16 17:39:29 +0800

    dwada

--- sql/table_create.sql
+++ sql/table_create.sql
@@ -6,7 +6,7 @@ CREATE  TABLE `mxkdatabase`.`TB_WEB_RESOURCE` (
   `sitename` VARCHAR(100) NULL ,
   `siteurl` VARCHAR(100) NULL ,
   `url` VARCHAR(150) NOT NULL ,
-  `info` VARCHAR(650) NULL ,
+  `info` VARCHAR(1024) NULL ,
   `multiinfo` VARCHAR(250) NULL ,
   `hits` INT NULL ,
   `posts` INT NULL ,
@@ -17,3 +17,17 @@ CREATE  TABLE `mxkdatabase`.`TB_WEB_RESOURCE` (
 ENGINE = MyISAM
 DEFAULT CHARACTER SET = utf8
 COMMENT = '互联网资源';
+
+
+CREATE  TABLE `mxkdatabase`.`TB_BASE_LINK` (
+  `id` INT NOT NULL AUTO_INCREMENT ,
+  `matchUrl` VARCHAR(250) NOT NULL ,
+  `url` VARCHAR(250) NOT NULL ,
+  PRIMARY KEY (`id`) ,
+  UNIQUE INDEX `url_UNIQUE` (`url` ASC) )
+ENGINE = MyISAM
+DEFAULT CHARACTER SET = utf8
+COMMENT = '基础爬取链接地址';
+
+insert into mxkdatabase.TB_BASE_LINK (matchUrl,url) 
+  values ('http://www.imx365.net/bbs/forum','http://www.imx365.net/bbs/forum-7-1.html');
