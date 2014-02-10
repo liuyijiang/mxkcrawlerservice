c6a9adecd8134a2868a405e803337edc8a227f67
liuyijiang <liuyijiang3430@qq.com>
2014-02-10 09:20:37 +0800

    dwada

---
+++ automatic/com/mxk/model/User.java
@@ -0,0 +1,53 @@
+package com.mxk.model;
+
+public class User {
+    private Integer id;
+
+    private String userName;
+
+    private String userImage;
+
+    private String userEmail;
+
+    private String userPassword;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName == null ? null : userName.trim();
+    }
+
+    public String getUserImage() {
+        return userImage;
+    }
+
+    public void setUserImage(String userImage) {
+        this.userImage = userImage == null ? null : userImage.trim();
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail == null ? null : userEmail.trim();
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword == null ? null : userPassword.trim();
+    }
+}
