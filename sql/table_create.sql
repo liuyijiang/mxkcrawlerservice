0365a4ad3eaebf1391992d9d4affbe5725488d34
liuyijiang <liuyijiang3430@qq.com>
2014-02-17 16:38:37 +0800

    dwad

--- sql/table_create.sql
+++ sql/table_create.sql
@@ -58,4 +58,63 @@ ENGINE = MyISAM
 DEFAULT CHARACTER SET = utf8
 COMMENT = '记录用户搜索的内容';
 
-INSERT INTO `mxkdatabase`.`tb_base_link` (`matchUrl`, `url`, `describes`, `state`, `ctime`) VALUES ('http://www.moxing.net/bbs/forum', 'http://www.moxing.net/bbs/forum-5-1.html', '', '1', now());
+CREATE  TABLE `mxkdatabase`.`tb_user_collect` (
+  `id` INT NOT NULL AUTO_INCREMENT ,
+  `user_id` INT NOT NULL ,
+  `collet_target` INT NOT NULL COMMENT '收藏目标id' ,
+  `collet_target_type` INT NOT NULL COMMENT '收藏目标类型(链接 图片)' ,
+  `create_time` DATETIME NOT NULL COMMENT '创建时间' ,
+  `tag` VARCHAR(50) NULL ,
+  `simple_desc` VARCHAR(100) NOT NULL COMMENT '简单收藏描述' ,
+  PRIMARY KEY (`id`) )
+ENGINE = MyISAM
+DEFAULT CHARACTER SET = utf8
+COMMENT = '用户收藏';
+
+
+CREATE  TABLE `mxkdatabase`.`tb_subject` (
+  `id` INT NOT NULL AUTO_INCREMENT ,
+  `category` VARCHAR(10) NOT NULL COMMENT '类型 | 原创 分享' ,
+  `faceimage` VARCHAR(100) NOT NULL COMMENT '封面图片' ,
+  `title` VARCHAR(100) NOT NULL COMMENT '标题' ,
+  `tag` VARCHAR(45) NOT NULL COMMENT '类型 舰船 航空' ,
+  `userid` INT NOT NULL ,
+  `createTime` DATETIME NOT NULL ,
+  PRIMARY KEY (`id`) )
+ENGINE = MyISAM
+DEFAULT CHARACTER SET = utf8
+COMMENT = '发布专题表';
+
+CREATE  TABLE `mxkdatabase`.`tb_part` (
+  `id` INT NOT NULL AUTO_INCREMENT ,
+  `subject_id` INT NOT NULL ,
+  `user_id` INT NOT NULL ,
+  `img_url` VARCHAR(100) NULL ,
+  `subject_name` VARCHAR(100) NOT NULL ,
+  `subject_type` VARCHAR(10) NOT NULL ,
+  `part_info` VARCHAR(255) NULL ,
+  `create_time` DATETIME NOT NULL ,
+  PRIMARY KEY (`id`) )
+ENGINE = MyISAM
+DEFAULT CHARACTER SET = utf8
+COMMENT = '专题部分';
+
+
+CREATE  TABLE `mxkdatabase`.`tb_catalog_resource` (
+  `id` INT NOT NULL AUTO_INCREMENT ,
+  `name` VARCHAR(45) NOT NULL COMMENT '名字' ,
+  `tag` VARCHAR(100) NULL COMMENT '标签 逗号隔开' ,
+  `type` INT NULL COMMENT '军舰 飞机' ,
+  `describes` VARCHAR(300) NULL COMMENT '简单描述' ,
+  `create_time` DATETIME NOT NULL ,
+  `image_url` VARCHAR(100) NULL COMMENT '图片链接' ,
+  `hot` INT NULL COMMENT '热度' ,
+  PRIMARY KEY (`id`) )
+ENGINE = MyISAM
+DEFAULT CHARACTER SET = utf8
+COMMENT = '编目后的资源 与tb_web_resource关联';
+
+
+INSERT INTO `mxkdatabase`.`tb_base_link` (`matchUrl`, `url`, `describes`, `state`, `ctime`) 
+VALUES 
+( 'http://www.militarymodelling.com/forums/threads.asp', 'http://www.militarymodelling.com/forums/threads.asp?t=93', 'MilitaryModelling', '1', now()); 
